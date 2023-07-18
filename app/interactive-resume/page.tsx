e6123804311697eafdf55821c270d0227aa3d805
Running water <alsgks1218@naver.com>
2023-07-18 23:38:46 +0900

    이미지 webp, jpg추가하고 폴백형태로 추가

--- app/interactive-resume/page.tsx
+++ app/interactive-resume/page.tsx
@@ -1,26 +1,38 @@
 "use client";
 
+import { useState, ReactEventHandler } from "react";
+import Image from "next/image";
 import { BsFillTerminalFill } from "react-icons/bs";
 import { motion } from "framer-motion";
 
 const Page = () => {
+  const [imageSrc, setImageSrc] = useState("/background.webp");
   return (
-    <>
+    <div style={{ height: "100vh", position: "relative" }}>
+      <Image
+        src={imageSrc}
+        alt="background"
+        fill={true}
+        style={{ objectFit: "cover" }}
+        onError={() => setImageSrc("/background.jpg")}
+      />
       <motion.div
         drag
         dragMomentum={false}
         whileDrag={{ cursor: "grabbing" }}
         style={{
+          position: "relative",
           width: 62,
           height: 62,
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          zIndex: 100,
         }}
       >
         <BsFillTerminalFill size={60} />
       </motion.div>
-    </>
+    </div>
   );
 };
 
