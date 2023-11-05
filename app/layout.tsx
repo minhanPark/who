81a75dcc11e826c446b3457a41158dfdd655d32a
Running water <alsgks1218@naver.com>
2023-11-05 23:06:43 +0900

    gtm 기본 코드 넣기

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Roboto } from "next/font/google";
 import createTitle from "./libs/create-title";
+import Script from "next/script";
 
 const roboto = Roboto({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
@@ -16,7 +17,28 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body className={roboto.className}>{children}</body>
+      <head>
+        <Script>
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N6MQN47T');
+        `}
+        </Script>
+      </head>
+      <body className={roboto.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N6MQN47T"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
