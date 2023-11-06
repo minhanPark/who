818c9520abc4e363788d0c1878ba8b78329bde4b
minhanPark <alsgks1218@naver.com>
2023-11-06 14:27:03 +0900

    add @next/third-parties/google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import { Roboto } from "next/font/google";
 import createTitle from "./libs/create-title";
-import Script from "next/script";
+// @ts-ignore
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const roboto = Roboto({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
@@ -17,28 +18,8 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head>
-        <Script>
-          {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N6MQN47T');
-        `}
-        </Script>
-      </head>
-      <body className={roboto.className}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N6MQN47T"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {children}
-      </body>
+      <body className={roboto.className}>{children}</body>
+      <GoogleTagManager gtmId="GTM-N6MQN47T" />
     </html>
   );
 }
