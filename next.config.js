e7fabf34f44566add92670531c7072ad328b0a31
minhanPark <alsgks1218@naver.com>
2024-04-11 11:43:30 +0900

    next.config.js에 headers 넣고 노션 이력서 s-maxage=1 테스트

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/notion-resume",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=59 max-age=0",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
