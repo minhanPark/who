a474ae47f7ba4db4cbadaecd6591c3762eda0045
minhanPark <alsgks1218@naver.com>
2024-06-07 15:20:27 +0900

    깃허브 폴더 아이콘 추가

--- app/components/modal/social-and-mail.tsx
+++ app/components/modal/social-and-mail.tsx
@@ -6,12 +6,17 @@ import { FcFeedback } from "react-icons/fc";
 import { ModalContainer } from "./modal-container";
 import type { Modal } from "./type";
 import { FolderIcon } from "../folder-icon";
+import { FaGithub } from "react-icons/fa";
 
 export function SocialAndMail({ closeModal, constraints, icon, title }: Modal) {
   const [isFocus, setIsFocus] = useState<string | null>(null);
 
   const sendMailToMe = () => {
     window.location.href = "mailto:alsgks1218@gmail.com";
+  };
+
+  const openGithub = () => {
+    window.open("https://github.com/minhanPark");
   };
 
   return (
@@ -35,6 +40,13 @@ export function SocialAndMail({ closeModal, constraints, icon, title }: Modal) {
           title="메일 보내기"
           icon={FcFeedback}
           triggerEvent={sendMailToMe}
+        />
+        <FolderIcon
+          handleFocus={setIsFocus}
+          isFocus={isFocus === "깃허브"}
+          title="깃허브"
+          icon={FaGithub}
+          triggerEvent={openGithub}
         />
       </div>
     </ModalContainer>
